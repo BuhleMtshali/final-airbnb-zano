8fddeb178a02b42f9a62bb66e22cfbc0536222f5
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-02 16:35:26 +0200

    added the frontend

--- airbnb-clone-z/backend/server.js
+++ airbnb-clone-z/backend/server.js
@@ -13,5 +13,5 @@ app.use("/api/accommodations", require("./routes/accommodationRoutes"));
 app.use("/api/reservations", require("./routes/reservationRoutes"));
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
-  app.listen(5000, () => console.log("Server running on port 5000"));
+  app.listen(5000, () => console.log("✅Server running on port 5000"));
 }).catch(err => console.log(err));
