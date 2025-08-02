bf1727bb0b61b5413932f0d77f19537d15c8dcf4
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-02 16:57:18 +0200

    created the fronted files

--- airbnb-clone-z/backend/routes/userRoutes.js
+++ airbnb-clone-z/backend/routes/userRoutes.js
@@ -5,4 +5,11 @@ const { loginUser, registerUser } = require("../controllers/userController");
 router.post("/login", loginUser);
 router.post("/register", registerUser);
 
+const protect = require("../middleware/auth");
+
+router.get("/profile", protect, (req, res) => {
+  res.send("You are authenticated 🎉");
+});
+
+
 module.exports = router;
