6b0b6e1aa77e537e6683ea44d82d6698513f5ccc
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-03 16:55:03 +0200

    updated the backedn

--- airbnb-clone-z/backend/routes/userRoutes.js
+++ airbnb-clone-z/backend/routes/userRoutes.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
+//const auth = require('../middleware/auth');
 const { loginUser, registerUser } = require("../controllers/userController");
 
 router.post("/login", loginUser);
 router.post("/register", registerUser);
+//router.post('/reservations', auth, createReservation);
 
 const protect = require("../middleware/auth");
 
 router.get("/profile", protect, (req, res) => {
   res.send("You are authenticated 🎉");
 });
+
+
 
 
 module.exports = router;
