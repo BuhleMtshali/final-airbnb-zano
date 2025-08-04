946bfe96b50d789440f6f7605066bc9b5a2071e6
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-04 07:38:15 +0200

    updated the backed

--- airbnb-clone-z/backend/server.js
+++ airbnb-clone-z/backend/server.js
@@ -5,13 +5,20 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+// 🔐 CORS fix
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
+// Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/accommodations", require("./routes/accommodationRoutes"));
 app.use("/api/reservations", require("./routes/reservationRoutes"));
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
-  app.listen(5000, () => console.log("✅Server running on port 5000"));
+  app.listen(5000, () => console.log("✅ Server running on port 5000"));
 }).catch(err => console.log(err));
